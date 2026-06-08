a035169dc45539e5dde3258f3fb16847004b5eb8
Thomas Lins <thomaslins43@gmail.com>
2026-06-08 19:12:41 -0300

    fix: att

--- Login/script.js
+++ Login/script.js
@@ -28,7 +28,7 @@ document.getElementById('btnLogin').addEventListener('click', function (e) {
     }
 });
 // Defina sua BASE_URL apontando para o seu backend
-const BASE_URL = 'http://financiello-backend.onrender.com/api/usuarios'; 
+const BASE_URL = "https://financiello-backend.onrender.com/api/usuarios"; 
 
 // Sua função de integração com a API
 async function create(data) {
